835b15a60cccb859da2af85652564a948eca7fc7
IndieCoderMM <hthant00chk@gmail.com>
2024-12-08 02:03:04 +0630

    Add day6 test

--- day6/solve_test.go
+++ day6/solve_test.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/IndieCoderMM/aoc-24/utils"
+)
+
+func TestSolve(t *testing.T) {
+	file := utils.GetFilePath(6)
+	expected := utils.GetAnswer(6)
+
+	t.Run("Day-6 Solution", func(t *testing.T) {
+		ans := Solve(file)
+		if ans != expected {
+			t.Errorf("Expected %d, got %d", expected, ans)
+		}
+	})
+}
